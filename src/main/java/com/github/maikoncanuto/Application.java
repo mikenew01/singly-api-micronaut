6af343f4001cc41d8a48e13826e0953fdb8e1470
Maikon Canuto <maikoncanuto@gmail.com>
2021-03-14 22:09:42 +0000

    Initial commit

---
+++ src/main/java/com/github/maikoncanuto/Application.java
@@ -0,0 +1,18 @@
+package com.github.maikoncanuto;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "singly-api-micronaut",
+            version = "0.0"
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
